c9714f8b2d7d37fc2c1110ffa0e019ab2a5d4b30
Nick Tustison <ntustison@gmail.com>
2017-02-27 15:57:31 -0800

    ENH:  slides.

---
+++ Slides/doIwpi2017Slides.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+#
+
+library(ggplot2)
+library(shiny)
+library(ggvis)
+library(rmarkdown)
+library( RISmed )
+
+srcdir<-"./"
+buildrmd='stitchedIwpiSlides.Rmd'
+
+rawrmds <- c( "formatIwpiSlides.Rmd",
+              "significance.Rmd",
+              "lung/itkLung.Rmd"
+               )
+
+for( x in 1:length( rawrmds ) ) {
+  if( x == 1 ) {
+    cmd <- paste( "cat ", rawrmds[x], " > ", buildrmd )
+  } else {
+  cmd<-paste( "cat ", rawrmds[x], " >> ", buildrmd )
+  }
+  system( cmd )
+}
+render( buildrmd, clean=TRUE )
